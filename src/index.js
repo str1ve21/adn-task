8faf0fcf20a5cef17b619da4f8f91d189efdcb8c
Michail <end3avour21@gmail.com>
2022-11-21 14:47:54 +0700

    header fix + anim

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import main from "../index.html";
 import clearInput from "./scripts/clearInput";
 
 import "./styles/reset.scss";
-import "./styles/index.scss";
+import "./styles/header.scss";
 
 clearInput("#clearHeaderSearch", "#headerSearch");
